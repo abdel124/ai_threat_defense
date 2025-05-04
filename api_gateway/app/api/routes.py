c78b7da1913ca90cad224a9ca874fca4c397b57f
abdel124 <touazi.abdelhak@gmail.com>
2025-05-04 11:09:40 -0700

    add JWTAuthenticator to routes api

--- api_gateway/app/api/routes.py
+++ api_gateway/app/api/routes.py
@@ -1,13 +1,13 @@
-from fastapi import APIRouter, Depends, Request
-from app.models.request import QueryRequest
-from app.services.llm_router import LLMRouter
-
-router = APIRouter()
+from app.utils.auth import JWTAuthenticator
+from fastapi import Header
 
 @router.post("/query")
 async def route_query(
     request_data: QueryRequest,
+    authorization: str = Header(...),
+    auth: JWTAuthenticator = Depends(get_jwt_authenticator),
     llm_router: LLMRouter = Depends(LLMRouter)
 ):
-    response = await llm_router.route_request(request_data)
-    return response
+    user_info = auth.verify_auth_header(authorization)
+    print("Verified user:", user_info)
+    return await llm_router.route_request(request_data)
