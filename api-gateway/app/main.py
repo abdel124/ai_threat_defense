6dbb0c96224d47833dea777b10dea4f2ea5faa80
abdel124 <touazi.abdelhak@gmail.com>
2025-06-07 08:49:53 -0700

    fix url for metrics expose

--- api-gateway/app/main.py
+++ api-gateway/app/main.py
@@ -1,17 +1,19 @@
-from fastapi import FastAPI
-from app.api.routes import router
+from fastapi import FastAPI, Request
 from prometheus_client import generate_latest, Counter
-from flask import Response
+from starlette.responses import Response
+from app.api.routes import router
 
 REQUEST_COUNTER = Counter('http_requests_total', 'Total number of HTTP requests')
 
 app = FastAPI(title="AI Threat Defense - API Gateway")
 app.include_router(router, prefix="/api")
 
-@app.before_request
-def before_request():
+@app.middleware("http")
+async def count_requests(request: Request, call_next):
     REQUEST_COUNTER.inc()
+    response = await call_next(request)
+    return response
 
-@app.route('/metrics')
+@app.get("/metrics")
 def metrics():
-    return Response(generate_latest(), mimetype='text/plain')
+    return Response(generate_latest(), media_type="text/plain")
