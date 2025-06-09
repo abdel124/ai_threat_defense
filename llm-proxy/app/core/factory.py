9b4e6085e12be2762004ba8ed0e2fea858974eac
abdel124 <touazi.abdelhak@gmail.com>
2025-06-08 17:29:33 -0700

    add env variable for api-key for openAI

--- llm-proxy/app/core/factory.py
+++ llm-proxy/app/core/factory.py
@@ -1,10 +1,14 @@
+import os
 from app.services.openai_client import OpenAIClient
 from app.services.local_client import LocalLLMClient
 from app.services.base import AbstractLLMClient
 
 def get_llm_backend(name: str) -> AbstractLLMClient:
     if name == "openai":
-        return OpenAIClient(api_key="")
+        api_key = os.getenv("OPENAI_API_KEY")
+        if not api_key:
+            raise ValueError("OPENAI_API_KEY is not set in the environment.")
+        return OpenAIClient(api_key=api_key)
     elif name == "local":
         return LocalLLMClient()
     else:
